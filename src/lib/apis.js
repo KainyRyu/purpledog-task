b0261191c6508c9a38a7791a44084a0c17a6463e
kainy ryu <kainy@joara.com>
2022-04-09 09:16:09 +0900

    api config

--- src/lib/apis.js
+++ src/lib/apis.js
@@ -6,56 +6,48 @@ export const BANNER_CODE = {
   sub2: 'MAIN_SUB_2', // 인증샷 배너
 };
 
+export const TEXT_KEY = {
+  countryWineTitle: 'COUNTRY_WINE_TITLE',
+  countryWineDesc: 'COUNTRY_WINE__DESCRIPTION',
+  eventDesc: 'EVENT_DESCRIPTION',
+  eventTitle: 'EVENT_TITLE',
+  recommendDesc: 'PERSONAL_RECOMMEND_DESCRIPTON',
+  recommnedTitle: 'PERSONAL_RECOMMEND_TITLE',
+  recommendProduct1: 'RECOMMEND_PRODUCT_1_TITLE',
+  recommendProduct2Title: 'RECOMMEND_PRODUCT_2_TITLTE',
+  recommendProduct2Desc: 'RECOMMEND_PRODUCT_2_DESCRIPTION',
+};
+
 export const getMainBanner = async (bannerCodeId) => {
-  try {
-    const res = axios.get(
-      `https://796f1d8e-8f52-4549-bad2-c1131d12efa7.mock.pstmn.io/:{{version}}/banner/bannerCodeId/${bannerCodeId}`,
-    );
-    return res;
-  } catch (err) {
-    console.log(err);
-  }
+  const fetch = await axios.get(
+    `https://796f1d8e-8f52-4549-bad2-c1131d12efa7.mock.pstmn.io/:{{version}}/banner/bannerCodeId/${bannerCodeId}`,
+  );
+  return fetch;
 };
 
 // MAIN
 export const getText = async (textGroupCode) => {
-  try {
-    const res = axios.get(
-      `https://796f1d8e-8f52-4549-bad2-c1131d12efa7.mock.pstmn.io/:{{version}}/text/textGroupCode/${textGroupCode}`,
-    );
-    return res;
-  } catch (err) {
-    console.log(err);
-  }
+  const fetch = await axios.get(
+    `https://796f1d8e-8f52-4549-bad2-c1131d12efa7.mock.pstmn.io/:{{version}}/text/textGroupCode/${textGroupCode}`,
+  );
+  return fetch;
 };
 
 export const getRecommend = async () => {
-  try {
-    const res = axios.get(
-      `https://796f1d8e-8f52-4549-bad2-c1131d12efa7.mock.pstmn.io/:{{version}}/product/selectTimeSaleWine`,
-    );
-    return res;
-  } catch (err) {
-    console.log(err);
-  }
+  const fetch = await axios.get(
+    'https://796f1d8e-8f52-4549-bad2-c1131d12efa7.mock.pstmn.io/:{{version}}/product/subscriptionWineRecommend',
+  );
+  return fetch;
 };
 export const getTimeSale = async () => {
-  try {
-    const res = axios.get(
-      'https://796f1d8e-8f52-4549-bad2-c1131d12efa7.mock.pstmn.io/:{{version}}/product/selectTimeSaleWine',
-    );
-    return res;
-  } catch (err) {
-    console.log(err);
-  }
+  const fetch = await axios.get(
+    'https://796f1d8e-8f52-4549-bad2-c1131d12efa7.mock.pstmn.io/:{{version}}/product/selectTimeSaleWine',
+  );
+  return fetch;
 };
 export const getPopular = async () => {
-  try {
-    const res = axios.get(
-      `https://796f1d8e-8f52-4549-bad2-c1131d12efa7.mock.pstmn.io/:{{version}}/product/selectPopularWine`,
-    );
-    return res;
-  } catch (err) {
-    console.log(err);
-  }
+  const fetch = await axios.get(
+    `https://796f1d8e-8f52-4549-bad2-c1131d12efa7.mock.pstmn.io/:{{version}}/product/selectPopularWine`,
+  );
+  return fetch;
 };
